da8283d8145fc28761feae85ff62a96fa027a40a
Liem Nguyen <liem.nguyen@eastagile.com>
2016-02-23 10:44:59 +0700

    lession - react-component-lifecycle-mounting-basics

--- App.js
+++ App.js
@@ -2,17 +2,53 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { val:0 };
+    this.update = this.update.bind(this);
+  }
+  update() {
+    this.setState({val: this.state.val + 1})
+  }
+  componentWillMount() {
+    console.log('mounting')
+  }
   render() {
-    return <Button> I <Heart/> you </Button>
+    console.log('rendering')
+    return <Button update={this.update}> I <Heart/> you {this.state.val} </Button>
+  }
+  componentDidMount() {
+    console.log('mounted')
+  }
+  componentWillUnmount() {
+    console.log('will unmount')
   }
 }
 
 class Button extends React.Component {
   render() {
-    return <button> {this.props.children} </button>
+    return <button onClick={this.props.update}> {this.props.children}  </button>
+  }
+}
+
+class Wrapper extends React.Component {
+  mount() {
+    ReactDom.render(<App />, document.getElementById('a'));
+  }
+  unmount() {
+    ReactDom.unmountComponentAtNode(document.getElementById('a'));
+  }
+  render () {
+    return (
+      <div>
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <button onClick={this.unmount.bind(this)}>Unmount</button>
+        <div id='a'></div>
+      </div>
+    )
   }
 }
 
 const Heart = () => <span className="glyphicon glyphicon-heart"> </span>
 
-export default App
+export default Wrapper
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import App from './App';
+import Wrapper from './App';
 
 ReactDom.render(
-  <App />, document.getElementById('app')
+  <Wrapper />, document.getElementById('app')
 );
